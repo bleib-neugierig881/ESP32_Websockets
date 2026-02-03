b6bb0b990241d66ad7a3ef794e05579ff0416bf3
bleib-neugierig881 <62718844+bleib-neugierig881@users.noreply.github.com>
2026-02-02 21:57:45 -0800

    Pushing initial template for future reference.

---
+++ ESP32_S3_ETH_Template/ESP32_S3_ETH_Template.ino
@@ -0,0 +1,29 @@
+/*
+Unique Board Settings for ESP32 S3 ETH
+Choose ESP32 S3 Dev Module
+USB CDC On Boot: Enabled
+CPU Freq: 240MHz
+Core Debug Level: Debug
+USB DFU On Boot: Disabled
+Erase All Flash..: Disabled
+Events Run on: Core 1
+Flash Mode: QIO 80MHz
+Flash Size 16MB (128Mb)
+JTAG: Disabled
+Arduino Runs on: Core 1
+USB Firmware... "Disabled"
+Partition Scheme: Huge APP (3MB No OTA/1MB SPIFFS)
+PSRAM: OPI PSRAM
+Upload Mode: UART0/Hardware CDC
+Upload Speed: 921600
+USB mode: Hardware CDC and JTAG
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
